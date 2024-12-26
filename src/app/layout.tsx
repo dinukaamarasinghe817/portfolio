5ca040f84bbf85966372b49b445a4d3029e5e820
Dinuka Ashan Amarasinghe <dinukaamarasinghe817@gmail.com>
2024-12-27 00:33:24 +0530

    Finalize content and responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+      </head>
       <body
         className={`${poppins.className} bg-black text-white antialiased flex flex-col justify-center items-center`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
 "use client";
-import { useEffect, useState, useRef, RefObject } from "react";
+import { useState, useRef, RefObject } from "react";
 import { Swiper , SwiperSlide } from 'swiper/react';
-// import SwiperCore from 'swiper';
-import Image from 'next/image';
 import { EffectCoverflow, Autoplay } from 'swiper/modules';
-
-// const swiper: SwiperType = new SwiperType({...});
-// SwiperCore.use([EffectCoverflow]);
+import TypeWriter from 'typewriter-effect';
+import Image from "next/image";
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-// import 'swiper/swiper-bundle.min.css';
-
-import openhack from '../assets/awards/openhack.png';
 
 type SocialLink = {
   name: string;
@@ -62,6 +56,11 @@ const socialLinks: SocialLink[] = [
     name: "Email",
     icon: "/mail.svg",
     url: "mailto:dinukaamarasinghe817@gmail.com"
+  },
+  {
+    name: "CV",
+    icon: "/cv.svg",
+    url: "/cv/cv.pdf"
   },
 ]
 
@@ -201,9 +200,7 @@ type Section = "home" | "projects" | "stack" | "blogs" | "awards";
 
 export default function Home() {
 
-  const [isTransitioning, setIsTransitioning] = useState(false);
   const [activeLink, setActiveLink] = useState<Section>("home");
-  // Use a Record to type the refs for each section
   const sectionsRef: Record<Section, RefObject<HTMLDivElement | null>> = {
     home: useRef<HTMLDivElement | null>(null),
     projects: useRef<HTMLDivElement | null>(null),
@@ -212,33 +209,7 @@ export default function Home() {
     awards: useRef<HTMLDivElement | null>(null),
   };
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     if (isTransitioning) return;
-  //     console.log("hellow");
-  //     const offsets = Object.entries(sectionsRef).map(([key, ref]) => ({
-  //       section: key as Section,
-  //       offsetTop: ref.current?.offsetTop ?? 0,
-  //     }));
-
-  //     const scrollPosition = window.scrollY + window.innerHeight / 30;
-
-  //     const currentSection = offsets
-  //       .reverse()
-  //       .find(({ offsetTop }) => scrollPosition >= offsetTop)?.section;
-
-  //     if (currentSection) setActiveLink(currentSection);
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, [sectionsRef, isTransitioning]);
-
   const handleNavigation = (section: Section) => {
-    setIsTransitioning(true);
     setActiveLink(section);
 
     const sectionRef = sectionsRef[section]?.current;
@@ -247,37 +218,36 @@ export default function Home() {
     }
 
     setTimeout(() => {
-      setIsTransitioning(false);
     }, 500);
   };
 
   return (
     <div className="w-full flex items-center justify-center bg-gradient-to-r from-transparent via-black to-transparent">
-    <div className="flex flex-col gap-12 pt-32 max-w-[640px] mx-4">
-      <nav className="fixed top-1/2 max-w-[52px] bg-grey left-[150px] transform -translate-y-1/2 flex flex-col gap-3 items-start overflow-x-show rounded-[50px] p-[6px] z-10">
+    <div className="flex flex-col gap-8 starter:gap-12 pt-32 max-w-[640px] mx-4">
+      <nav className="fixed left-1/2 bg-opacity-50 backdrop-blur-md bottom-4 navdesktop:top-1/2 navdesktop:max-w-[52px] bg-lightGrey navdesktop:left-[50px] navwide:left-[150px] transform -translate-x-1/2 navdesktop:h-fit navdesktop:-translate-y-1/2 flex navdesktop:flex-col gap-3 items-start overflow-x-show rounded-[50px] p-[6px] z-10">
         <a 
         onClick={() => handleNavigation("home" as Section)}
-        className="flex flex-row gap-4 items-center justify-start rounded-full transition cursor-pointer">
+        className="flex flex-row gap-2 starter:gap-4 items-center justify-start rounded-full transition cursor-pointer">
           <div className={`group text-white min-w-10 text-xl rounded-[50px]  p-2 ${activeLink === 'home' ? 'bg-white' : 'bg-transparent'} hover:bg-white transition-colors duration-200 ease-in-out`}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path className={`${activeLink === 'home' ? 'stroke-black' : 'stroke-white'} group-hover:stroke-black transition-colors duration-200 ease-in-out`} d="M18 3C17.2044 3 16.4413 3.31607 15.8787 3.87868C15.3161 4.44129 15 5.20435 15 6V18C15 18.7956 15.3161 19.5587 15.8787 20.1213C16.4413 20.6839 17.2044 21 18 21C18.7956 21 19.5587 20.6839 20.1213 20.1213C20.6839 19.5587 21 18.7956 21 18C21 17.2044 20.6839 16.4413 20.1213 15.8787C19.5587 15.3161 18.7956 15 18 15H6C5.20435 15 4.44129 15.3161 3.87868 15.8787C3.31607 16.4413 3 17.2044 3 18C3 18.7956 3.31607 19.5587 3.87868 20.1213C4.44129 20.6839 5.20435 21 6 21C6.79565 21 7.55871 20.6839 8.12132 20.1213C8.68393 19.5587 9 18.7956 9 18V6C9 5.20435 8.68393 4.44129 8.12132 3.87868C7.55871 3.31607 6.79565 3 6 3C5.20435 3 4.44129 3.31607 3.87868 3.87868C3.31607 4.44129 3 5.20435 3 6C3 6.79565 3.31607 7.55871 3.87868 8.12132C4.44129 8.68393 5.20435 9 6 9H18C18.7956 9 19.5587 8.68393 20.1213 8.12132C20.6839 7.55871 21 6.79565 21 6C21 5.20435 20.6839 4.44129 20.1213 3.87868C19.5587 3.31607 18.7956 3 18 3Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           </div>
-          <span className={`${activeLink === 'home' ? 'flex' : 'hidden'}`}>Home</span>
+          <span className={`${activeLink === 'home' ? 'flex' : 'hidden'} text-[14px] starter:text-16px`}>Home</span>
         </a>
         <a 
         onClick={() => handleNavigation("projects" as Section)}
-        className="flex flex-row gap-4 items-center justify-start mb-0 rounded-full transition cursor-pointer">
+        className="flex flex-row gap-2 starter:gap-4 items-center justify-start mb-0 rounded-full transition cursor-pointer">
           <div className={`group text-white min-w-10 text-xl rounded-[50px]  p-2 ${activeLink === 'projects' ? 'bg-white' : 'bg-transparent'} hover:bg-white transition-colors duration-200 ease-in-out`}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path className={`${activeLink === 'projects' ? 'fill-black' : 'fill-white'} group-hover:fill-black transition-colors duration-200 ease-in-out`} fill="white" fillRule="evenodd" clipRule="evenodd" d="M9.11226 4.97114C8.49688 5.53789 8 6.47301 8 8C8 8.92872 8.06316 9.46011 8.13193 9.76728C8.1886 10.0204 8.24081 10.0867 8.25153 10.1003L8.25286 10.1021C8.30316 10.1698 8.32516 10.191 8.38697 10.2508L8.38698 10.2508L8.38699 10.2508C8.44706 10.3089 8.54475 10.4034 8.74255 10.6121C9.07553 10.9636 9.446 11.4199 9.78885 12.1056C10.1542 12.8362 10.4103 13.4678 10.5896 14H13.4104C13.5897 13.4678 13.8458 12.8362 14.2111 12.1056C14.554 11.4199 14.9245 10.9636 15.2575 10.6121C15.4552 10.4034 15.5529 10.3089 15.613 10.2508L15.613 10.2508L15.613 10.2508L15.613 10.2508L15.613 10.2508C15.6748 10.191 15.6968 10.1698 15.7471 10.1021L15.7485 10.1003C15.7592 10.0867 15.8114 10.0204 15.8681 9.76728C15.9368 9.46011 16 8.92872 16 8C16 6.47301 15.5031 5.53789 14.8877 4.97114C14.2454 4.37957 13.2658 4 12 4C10.7342 4 9.75457 4.37957 9.11226 4.97114ZM6 8C6 4 8.68629 2 12 2C15.3137 2 18 4 18 8C18 10.6936 17.5196 11.1738 16.9185 11.7746C16.627 12.0659 16.3072 12.3857 16 13C15.5714 13.8571 15.3265 14.5306 15.1866 15.0204C15.0349 15.5514 14.5523 16 14 16H10C9.44772 16 8.96513 15.5514 8.81341 15.0204C8.67346 14.5306 8.42857 13.8571 8 13C7.69283 12.3857 7.37296 12.0659 7.08146 11.7746C6.48044 11.1738 6 10.6936 6 8ZM9 17.5C9 17.2239 9.22386 17 9.5 17H14.5C14.7761 17 15 17.2239 15 17.5V19.741C15 19.9034 14.9217 20.0554 14.7855 20.1439C14.4235 20.3789 13.6706 20.8342 13.0532 20.9875C13.0178 20.9963 12.9812 21 12.9448 21H11.0549C11.0184 21 10.9822 20.9963 10.9468 20.9875C10.3294 20.8342 9.57651 20.3789 9.21455 20.1439C9.07833 20.0554 9 19.9034 9 19.741V17.5ZM11 7.50846L11 7.50837C10.9955 8.05679 10.5495 8.49999 10 8.49999C9.44771 8.49999 9 8.05227 9 7.49999C9 6.98675 9.22666 6.39714 9.59413 5.9378C9.98695 5.44678 10.6283 5 11.5 5C12.0523 5 12.5 5.44772 12.5 6C12.5 6.55228 12.0523 7 11.5 7C11.3717 7 11.263 7.05321 11.1559 7.18719C11.1001 7.2569 11.0564 7.33682 11.0282 7.41068C11.008 7.4639 11.0021 7.4968 11.0005 7.50588C11.0002 7.5076 11.0001 7.50847 11 7.50846Z"/>
             </svg>
           </div>
-          <span className={`${activeLink === 'projects' ? 'flex' : 'hidden'}`}>Projects</span>
+          <span className={`${activeLink === 'projects' ? 'flex' : 'hidden'} text-[14px] starter:text-16px`}>Projects</span>
         </a>
         <a 
         onClick={() => handleNavigation("stack" as Section)}
-        className="flex flex-row gap-4 items-center justify-start mb-0 rounded-full transition cursor-pointer">
+        className="flex flex-row gap-2 starter:gap-4 items-center justify-start mb-0 rounded-full transition cursor-pointer">
           <div className={`group text-white min-w-10 text-xl rounded-[50px]  p-2 ${activeLink === 'stack' ? 'bg-white' : 'bg-transparent'} hover:bg-white transition-colors duration-200 ease-in-out`}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path className={`${activeLink === 'stack' ? 'stroke-black' : 'stroke-white'} group-hover:stroke-black transition-colors duration-200 ease-in-out`} d="M12 2L2 7L12 12L22 7L12 2Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
@@ -285,21 +255,21 @@ export default function Home() {
               <path className={`${activeLink === 'stack' ? 'stroke-black' : 'stroke-white'} group-hover:stroke-black transition-colors duration-200 ease-in-out`} d="M2 12L12 17L22 12" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           </div>
-          <span className={`${activeLink === 'stack' ? 'flex' : 'hidden'}`}>Stack</span>
+          <span className={`${activeLink === 'stack' ? 'flex' : 'hidden'} text-[14px] starter:text-16px`}>Stack</span>
         </a>
         <a 
         onClick={() => handleNavigation("blogs" as Section)}
-        className="flex flex-row gap-4 items-center justify-start mb-0 rounded-full transition cursor-pointer">
+        className="flex flex-row gap-2 starter:gap-4 items-center justify-start mb-0 rounded-full transition cursor-pointer">
           <div className={`group text-white min-w-10 text-xl rounded-[50px]  p-2 ${activeLink === 'blogs' ? 'bg-white' : 'bg-transparent'} hover:bg-white transition-colors duration-200 ease-in-out`}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path className={`${activeLink === 'blogs' ? 'stroke-black' : 'stroke-white'} group-hover:stroke-black transition-colors duration-200 ease-in-out`} d="M13.228 7.29901L17.3995 11.4706L10.1147 18.7554L4.25981 20.3676L5.87201 14.5128L15.5741 4.81071C15.7208 4.71895 16.1496 4.48531 16.7236 4.45162C17.2964 4.41801 18.1304 4.57406 19.0919 5.53553C20.0534 6.49701 20.2094 7.33099 20.1758 7.90381C20.1421 8.47783 19.9085 8.90663 19.8167 9.05335L18.8137 10.0564L14.6422 5.88479L13.228 7.29901ZM15.5924 4.79238C15.5928 4.79197 15.5928 4.79203 15.5923 4.79248L15.5924 4.79238ZM19.8351 9.03502L19.835 9.03512C19.8352 9.03487 19.8353 9.03474 19.8353 9.03474C19.8353 9.03474 19.8352 9.03483 19.8351 9.03502Z" strokeWidth="2"/>
             </svg>
           </div>
-          <span className={`${activeLink === 'blogs' ? 'flex' : 'hidden'}`}>Blogs</span>
+          <span className={`${activeLink === 'blogs' ? 'flex' : 'hidden'} text-[14px] starter:text-16px`}>Blogs</span>
         </a>
         <a 
         onClick={() => handleNavigation("awards" as Section)}
-        className="flex flex-row gap-4 items-center justify-start mb-0 rounded-full transition cursor-pointer">
+        className="flex flex-row gap-2 starter:gap-4 items-center justify-start mb-0 rounded-full transition cursor-pointer">
           <div className={`group text-white min-w-10 text-xl rounded-[50px]  p-2 ${activeLink === 'awards' ? 'bg-white' : 'bg-transparent'} hover:bg-white transition-colors duration-200 ease-in-out`}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path className={`${activeLink === 'awards' ? 'stroke-black' : 'stroke-white'} group-hover:stroke-black transition-colors duration-200 ease-in-out`} d="M12.5 17C10.8264 17 9.36971 18.265 8.61766 20.1312C8.25846 21.0225 8.77389 22 9.45877 22H15.5412C16.2261 22 16.7415 21.0225 16.3823 20.1312C15.6303 18.265 14.1736 17 12.5 17Z" strokeWidth="2" strokeLinecap="round"/>
@@ -308,29 +278,43 @@ export default function Home() {
               <path className={`${activeLink === 'awards' ? 'stroke-black' : 'stroke-white'} group-hover:stroke-black transition-colors duration-200 ease-in-out`} d="M12.5 17C15.5208 17 18.065 12.3379 18.8297 5.99089C19.0412 4.23558 19.147 3.35793 18.5868 2.67896C18.0267 2 17.1223 2 15.3134 2H9.68658C7.87775 2 6.97333 2 6.41317 2.67896C5.85301 3.35793 5.95875 4.23558 6.17025 5.99089C6.935 12.3379 9.47923 17 12.5 17Z" strokeWidth="2" strokeLinecap="round"/>
             </svg>
           </div>
-          <span className={`${activeLink === 'awards' ? 'flex' : 'hidden'}`}>Awards</span>
+          <span className={`${activeLink === 'awards' ? 'flex' : 'hidden'} text-[14px] starter:text-16px`}>Awards</span>
         </a>
       </nav>
 
-      <section id="home" ref={sectionsRef.home}>
-        <img src="/picture.png" alt="" />
+      <section id="home" ref={sectionsRef.home} className="flex justify-center">
+        <img className="h-[200px] starter:h-[300px] w-auto rounded-b-[150px]" src="/picture.png" alt="" />
       </section>
 
-      <section className="flex flex-col">
-        <h1 className="font-medium text-[60px]">Hey, I’m Dinuka</h1>
-        <h1 className="font-medium text-[60px] -mt-4
+      <section className="flex flex-col -mb-4 items-center starter:items-start">
+        <h1 className="font-medium text-[36px] starter:text-[60px]">Hey, I’m Dinuka</h1>
+        <h1 className="font-medium text-[32px] starter:text-[60px] -mt-2 starter:-mt-4
         bg-gradient-to-br from-gradientOrange to-gradientPurple bg-clip-text text-transparent
-        ">Fullstack Developer.</h1>
+        ">
+          <TypeWriter
+          onInit={(typewriter) =>{
+            typewriter
+            .typeString("Fullstack Developer.")
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString("Illustrator Artist.")
+            .pauseFor(2000)
+            .deleteAll()
+            .start();
+          }}
+          options={{ loop: true }}
+          />
+        </h1>
       </section>
 
       <section className="flex flex-col">
-        <ul className="flex flex-row gap-9">
+        <ul className="flex flex-row gap-2 starter:gap-4 justify-center starter:justify-start">
           {socialLinks.map((socialLink, index) => (
             <li className="flex" key={index}>
               <a href={socialLink.url} 
-              className="flex flex-row gap-2.5 py-2.5 px-6 bg-grey rounded-[50px] hover:bg-lightGrey transition-colors duration-200 ease-in-out">
+              className="flex flex-row gap-1.5 starter:gap-2.5 starter3:py-2.5 starter:py-2.5 py-2 px-3 starter3:px-[16px] starter:px-6 bg-grey rounded-[50px] hover:bg-lightGrey transition-colors duration-200 ease-in-out">
                 <img src={socialLink.icon} alt="" className="w-4 h-auto"/>
-                <p>{socialLink.name}</p>
+                <p className="text-[12px] starter:text-[14px]">{socialLink.name}</p>
               </a>
             </li>
           ))}
@@ -351,7 +335,7 @@ export default function Home() {
         <h2 className="font-semibold text-[20px]">My Timeline</h2>
         
         <div className="relative mt-8">
-          <div className="absolute left-1/2 top-0 h-[calc(100%-70px)] w-0.5 bg-white transform -translate-x-1/2"></div>
+          <div className="absolute left-1/2 top-0 h-[calc(100%-150px)] starter3:h-[calc(100%-130px)] starter2.75:h-[calc(100%-108px)] starter2.5:h-[calc(100%-86px)] starter2:h-[calc(100%-86px)] starter1:h-[calc(100%-70px)] w-0.5 bg-white transform -translate-x-1/2"></div>
 
           <div className="flex items-start mb-12">
             <div className="w-1/2"></div>
@@ -366,8 +350,8 @@ export default function Home() {
                 </div>
                 <p className="text-[14px]">A final year Computer Science undergraduate 
                   <span className="text-linkColor">
-                    <a href="https://ucsc.cmb.ac.lk/"> @UCSC</a>
-                  </span>
+                    <a href="https://ucsc.cmb.ac.lk/"> @UCSC </a>
+                  </span> with 3.92 GPA
                 </p>
               </div>
             </div>
@@ -510,11 +494,11 @@ export default function Home() {
 
       <section id="stack" ref={sectionsRef.stack}>
         <h2 className="font-semibold text-[20px]">Technologies</h2>
-        <div className="grid gap-3 rounded-lg sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mt-8">
+        <div className="grid gap-3 rounded-lg grid-cols-3 starter2:grid-cols-4 starter:grid-cols-5 mt-8">
           {technologies.map((technology, index) => (
-              <div key={index} className="flex flex-col justify-center items-center bg-grey p-4 rounded-md w-[118px] h-[118px] hover:bg-lightGrey transition-colors duration-300">
-                <img src={technology.icon} alt="React" className="w-12 h-12 mb-2" />
-                <p className="text-white">{technology.name}</p>
+              <div key={index} className="flex flex-col justify-center items-center bg-grey p-4 rounded-md  starter3:w-[118px] starter3:h-[118px] hover:bg-lightGrey transition-colors duration-300">
+                <img src={technology.icon} alt="React" className="w-7 h-7 starter3:w-12 starter3:h-12 mb-2" />
+                <p className="text-white text-[12px] starter3:text-[16px]">{technology.name}</p>
               </div>
           ))}
         </div>
@@ -560,7 +544,7 @@ export default function Home() {
             modifier: 6.5,
           }}
           autoplay={{
-            delay: 1500,
+            delay: 2000,
             disableOnInteraction: false,
           }}
           modules={[EffectCoverflow, Autoplay]}
@@ -571,7 +555,7 @@ export default function Home() {
             key={index}
             className={`w-fit relative`}
             >
-              <img className="rounded-[8px] w-[400px] h-[250px]" src={award.imagePath} alt="slide_image" />
+              <img className="rounded-[8px] w-[300px] h-[200px] starter:w-[400px] starter:h-[250px]" src={award.imagePath} alt="slide_image" />
               <div className="absolute rounded-b-[8px] left-0 bottom-0 p-2 bg-gradient-to-b from-transparent to-black w-full h-[150px] flex flex-col items-start justify-end">
                 <h3>{award.title}</h3>
                 <p className="text-[12px]">{award.name}</p>
@@ -581,7 +565,7 @@ export default function Home() {
         </Swiper>
       </section>
 
-      <section className="mb-12">
+      <section className="mb-[90px] navdesktop:mb-12">
 
       </section>
 
